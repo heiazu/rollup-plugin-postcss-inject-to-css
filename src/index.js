abe0c858e35ca6f61efc4a72c14b08613dfd15ab
zhuminghui2 <zhuminghui2@jd.com>
2021-03-17 10:45:35 +0800

    fix: 修复css重复引入导致冲突问题

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ export default function inlineToExtract (options = {}) {
       if (!filter(id)) return null
     },
     generateBundle (options_, bundle) {
+      const resolveCss = []
       Object.keys(bundle).forEach(name => {
         const bundleItem = bundle[name]
         bundleItem.imports.forEach((item, index) => {
@@ -17,12 +18,15 @@ export default function inlineToExtract (options = {}) {
             code = /"[\s\S^"]*"/igm.exec(bundle[item].code)
             if (code[0]) {
               code = code[0].replace(/\\n/g, '').replace(/\\"/g, '"')
-              Object.assign(bundle[item], {
-                fileName: item.replace(/\.(scss|less)\.js/, '.css'),
-                code: code.slice(1, code.length - 1),
-                importedBindings: null,
-                imports: []
-              })
+              if (!resolveCss.includes(item)) {
+                Object.assign(bundle[item], {
+                  fileName: item.replace(/\.(scss|less)\.js/, '.css'),
+                  code: code.slice(1, code.length - 1),
+                  importedBindings: null,
+                  imports: []
+                });
+                resolveCss.push(item)
+              }
               delete bundleItem.importedBindings[item]
               bundleItem.importedBindings[item.replace(/\.(scss|less)\.js/, '.css')] = []
               bundleItem.imports[index] = bundleItem.imports[index].replace(/\.(scss|less)\.js/, '.css')
